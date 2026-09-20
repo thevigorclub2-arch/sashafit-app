11c241d9849d07564196d3577188e61bd27a11c1
thevigorclub2-arch <thevigorclub2@gmail.com>
2026-09-20 15:49:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.SASHAFIT = {
 
   // Supabase: Project Settings -> API -> публічний ключ (anon або publishable).
   // Ключ service_role / secret сюди НЕ вставляти.
-  SUPABASE_ANON_KEY: "ВСТАВ-ПУБЛІЧНИЙ-КЛЮЧ",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxeGxsZGNxbnFzYnR0cnZneWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NzA4MjEsImV4cCI6MjEwNTQ0NjgyMX0._jN8HNtDBSKgWryR3MoHQImTTCTptwuAJLFWm0Dt40w",
 
   // Посилання-запрошення для клієнтів: https://t.me/USERNAME_БОТА/КОРОТКА_НАЗВА?startapp=
   // КОРОТКА_НАЗВА задається в BotFather при створенні застосунку командою /newapp.
